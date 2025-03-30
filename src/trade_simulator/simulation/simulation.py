b56ed6eab04d24602a5cf06e1ded07ac5d424d5e
caesiumCS <caesiummm@yandex.ru>
2025-03-31 00:08:00 +0500

    Add return statement

--- src/trade_simulator/simulation/simulation.py
+++ src/trade_simulator/simulation/simulation.py
@@ -11,11 +11,13 @@ class Simulation:
 
         self.pools = self.create_pools(self.simulation_build_args["pools_settings"])
 
+
     def create_pools(self, pools_settings: Dict[str, Any]) -> List[Pool]:
         check_pools_settings(pools_settings)
         pools = []
         for pool_settings in pools_settings["pools"]:
             pools.append(Pool(**pool_settings))
+        return pools
 
 
 
