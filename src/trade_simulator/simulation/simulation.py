e898a531c0ded6c1a19c73b6545374799af649a4
caesiumCS <caesiummm@yandex.ru>
2025-03-30 23:06:50 +0500

    Basic functionality

--- src/trade_simulator/simulation/simulation.py
+++ src/trade_simulator/simulation/simulation.py
@@ -1,3 +1,8 @@
+from trade_simulator.pool.pool import Pool
+
 class Simulation:
     def __init__(self, **kwargs):
-        pass
+        self.pools = []
+        self.agents = []
+        print(kwargs)
+
