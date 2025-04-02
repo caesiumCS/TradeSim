d939482efad475c10cbc8f72fd573e0e659f7762
caesiumCS <caesiummm@yandex.ru>
2025-04-02 17:56:44 +0500

    Order development

--- src/trade_simulator/order/order.py
+++ src/trade_simulator/order/order.py
@@ -1,16 +1,22 @@
+from typing import Optional
+
 from trade_simulator.utils.consts import ORDER_OPERATION_TYPES
 
 class Order:
     def __init__(self,
                  trader_id:int,
-                 timestamp:int,
+                 creation_timestamp:int,
                  operation_type: str,
                  token: str,
-                 priority:int = 1):
+                 priority:int = 1,
+                 cancel_possibility: bool = True,
+                 lifetime: Optional[int] = None):
         self.trader_id = trader_id
-        self.timestamp = timestamp
+        self.creation_timestamp = creation_timestamp
         self.token = token
         self.priority = priority
+        self.cancel_possibility = cancel_possibility
+        self.lifetime = lifetime
 
         if operation_type not in ORDER_OPERATION_TYPES:
             raise ValueError(f"Unsupported operation type {operation_type}.")
