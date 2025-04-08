b2cf3133cddcdbfa8129ff0e8989c90f7b5f3bc2
caesiumCS <caesiummm@yandex.ru>
2025-04-08 23:47:21 +0500

    Refactoring and new configs

--- src/trade_simulator/agents/basic_agent.py
+++ src/trade_simulator/agents/basic_agent.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 
+
 class BasicAgent(ABC):
     def __init__(self, **kwargs):
+        # TODO - add basic kwargs checker
         self.pools = {}
 
     @abstractmethod
-    def run_agent_action(self, timestamp:int):
+    def run_agent_action(self, timestamp: int):
         pass
 
     @abstractmethod
--- src/trade_simulator/agents/single_pool_random_trader.py
+++ src/trade_simulator/agents/single_pool_random_trader.py
@@ -1,18 +1,15 @@
 from trade_simulator.agents.basic_agent import BasicAgent
 
+
 class SinglePoolRandomTrader(BasicAgent):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # self.chance_to_action = kwargs["chance_to_action"]
+        # TODO - check input parameters
+
         self.portfolio = kwargs["portfolio"]
+        self.steps_to_make_new_transaction = kwargs["steps_to_make_new_transaction"]
         self.pool_id = [kwargs["pool_id"]]
 
-    def check_settings(self, **kwargs):
-        self.chance_to_action = kwargs["chance_to_action"]
-        if self.chance_to_action <= 0 or self.chance_to_action > 100:
-            raise ValueError(f"Got 'chance_to_action' equal to {self.chance_to_action}.")
-
-
-    def run_agent_action(self, timestamp:int):
+    def run_agent_action(self, timestamp: int):
         pass
